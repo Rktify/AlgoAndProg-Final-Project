42ce533e06c7666421fe182512b11627b3b1565b
Wilbert <91464358+Rktify@users.noreply.github.com>
2022-01-12 11:57:26 +0700

    Update Minesweeper.py

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,20 +1,21 @@
 from MineClass import *
+from BoardClass import *
 
-h = int(input("How many blocks (height) do you want to have in the board? (minimal 5): ")) #height of board
-while h < 5:
-    h = int(input("How many blocks (height) do you want to have in the board? (minimal 5): "))
+h = int(input("How many blocks (height) do you want to have in the board? (min. 5, max. 30): ")) #height of board
+while h < 5 or h > 30:
+    h = int(input("How many blocks (height) do you want to have in the board? (min. 5, max. 30): "))
 
-w = int(input("How many blocks (width) do you want to have in the board? (minimal 5): ")) #width of baord
-while w < 5:
-    w = int(input("How many blocks (height) do you want to have in the board? (minimal 5): "))
+w = int(input("How many blocks (width) do you want to have in the board? (min. 5, max. 30): ")) #width of baord
+while w < 5 or h > 30:
+    w = int(input("How many blocks (height) do you want to have in the board? (min. 5, max. 30): "))
 
 size = (h, w) #The size of the board, i usually play 20x20
 
-prob = float(input("Whats the bomb probability u want to have? (in decimal, (i.e. 0.1 is 10%)) (min 0.05, max 0.9): ")) #The probability of there being a bomb
+prob = float(input("Whats the bomb probability u want to have? (in decimal, (i.e. 0.1 is 10%)) (min 0.1, max 0.9): ")) #The probability of there being a bomb
 while prob < 0.1 or prob > 0.9:
-    prob = float(input("Whats the bomb probability u want to have? (in decimal, (i.e. 0.1 is 10%)) (min 0.05, max 0.9): "))
+    prob = float(input("Whats the bomb probability u want to have? (in decimal, (i.e. 0.1 is 10%)) (min 0.1, max 0.9): "))
 
 board = Board(size, prob) #To set up the board
-SSize = (1000, 900) #The screen size
+SSize = (1000, 1000) #The screen size
 mine = Game(SSize , board)
 mine.game() #The main run function
