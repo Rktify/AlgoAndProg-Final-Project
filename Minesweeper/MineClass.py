2116dcd9392ad63102ce9e14f01ebb2f91586c19
Wilbert <91464358+Rktify@users.noreply.github.com>
2022-01-12 11:57:04 +0700

    separated classes to their each file

--- Minesweeper/MineClass.py
+++ Minesweeper/MineClass.py
@@ -1,6 +1,7 @@
 import pygame
 import os
-from random import random
+from PiecesClass import *
+from BoardClass import *
 
 class Game:
     def __init__(self, SSize, board): #Initializer
@@ -16,11 +17,11 @@ class Game:
         run = True
         while run: #basically while game is running
             for event in pygame.event.get():
-                if (event.type == pygame.QUIT):
+                if (event.type == pygame.QUIT): #Quitting the gmae
                     run = False
                 if (event.type == pygame.MOUSEBUTTONDOWN): #Click
                     position = pygame.mouse.get_pos()
-                    rightClick = pygame.mouse.get_pressed()[2]
+                    rightClick = pygame.mouse.get_pressed()[2] #Index 2 is for right click
                     self.Click(position, rightClick)
             self.grid()
             pygame.display.flip()
@@ -29,7 +30,7 @@ class Game:
                 run = False #Closes game
             elif self.board.getLose(): #Lose state
                 print("You Lost :(")
-                run = False
+                run = False #Closes game
         pygame.quit()
 
     def grid(self): #Draw the board
@@ -61,122 +62,14 @@ class Game:
             if piece.getFlagged():
                 string = "flag" #This is for the flagged status
             else:
-                string = "empty"
+                string = "empty" #If its not opened, and not flagged, then yea its empty =D
         # if piece.getBomb():
         #     string = "bomb"
         # else:
         #     string = str(piece.getNumAround())
-        return self.imgs[string]
+        return self.imgs[string] #Returns the image so it can be placed to each block
 
     def Click(self, position, rightClick):
         coords = position[1] // self.gridsize[1], position[0] // self.gridsize[0] #Getting the coordinate for the blocks
         piece = self.board.getPiece(coords) #Gets the piece based on the position (coordinates, in pygame top left is (0, 0))
         self.board.Click(piece, rightClick)
-
-
-
-class Board: #This is the Board class, mostly for stuff around the board
-    def __init__(self, size, prob): #THis is the Initializer of the code
-        self.size = size #The size of the game (blocks)
-        self.prob = prob #The probability of the block being a bomb
-        self.lose = False #Lose obviously false in the beginning or else already GG we lose =D
-        self.numOpened = 0 #The number of opened Blocks
-        self.setBoard()
-
-
-    def setBoard(self): #Setting up the board
-        self.numNotBombs = 0 #The number of blocks that does not have a bomb in them
-        self.board = []
-        for row in range(self.size[0]):
-            row = []
-            for col in range(self.size[1]):
-                Bomb = random() < self.prob #Bombs
-                if not Bomb:
-                    self.numNotBombs += 1 #Every block that isnt a bomb, numNotBombs + 1
-                piece = Pieces(Bomb) #pushes the bomb to piece
-                row.append(piece)
-            self.board.append(row)
-        self.setNumbers()
-
-    def setNumbers(self): #setting the numbers
-        for row in range(self.size[0]):
-            for col in range(self.size[1]):
-                piece = self.getPiece((row, col)) #getting the pieces for each coods
-                numbers = self.getNumList((row, col)) #getting the numbers for each coods
-                piece.setNumbers(numbers) #Setting each piece's numbers based on the list of numbers that it got from getNumList
-
-    def getNumList(self, coords): #Getting the numbers for the adjacent blocks
-        numbers = []
-        for row in range(coords[0] - 1, coords[0] + 2):
-            for col in range(coords[1] - 1, coords[1] + 2):
-                outOfBounds = row < 0 or col < 0 or row >= self.size[0]  or col >= self.size[1]
-                if outOfBounds:
-                    continue
-                numbers.append(self.getPiece((row, col)))
-        return numbers
-
-    def getSize(self): #Self explanatory get funcs
-        return self.size
-
-    def getPiece(self, index):
-        return self.board[index[0]][index[1]]
-
-    def getLose(self):
-        return self.lose
-
-    def getWon(self):
-        return  self.numNotBombs == self.numOpened #If the number of blocks that doesnt have a bomb in them = the number of blocks that are opened, we win
-
-    def Click(self, piece, flag):
-        if piece.getOpened() or (not flag and piece.getFlagged()): #If the piece is opened and not flagged, we cna click
-            return
-        elif flag:
-            piece.toggleFlag() #if we right clicked we flag the block
-            return
-        piece.click()
-        if piece.getBomb():
-            self.lose = True #if block has bomb we lose
-            return
-        self.numOpened += 1
-        if piece.getNumAround() != 0: #Checks if a neighboring block is a 0 or not, to continue to the function below
-            return
-        for i in piece.getNumbers(): #If the block is 0 , itll keep opening until it reaches a block that isnt a bomb or an opened block
-            if not i.getBomb() and not i.getOpened():
-                self.Click(i, False)
-
-class Pieces:
-    def __init__(self, Bomb): #Initializer
-        self.Bomb = Bomb
-        self.opened = False
-        self.flagged = False
-
-    def getBomb(self): #Self explanatory get funcs
-        return self.Bomb
-
-    def getOpened(self):
-        return self.opened
-
-    def getFlagged(self):
-        return self.flagged
-
-    def getNumbers(self):
-        return self.numbers
-
-    def getNumAround(self):
-        return self.numAround
-
-    def toggleFlag(self): #toggleing the rightClick flag
-        self.flagged = not self.flagged
-
-    def click(self): #Handle click
-        self.opened = True
-
-    def setNumbers(self, numbers):
-        self.numbers = numbers
-        self.setNumAround()
-
-    def setNumAround(self): #setting the number according the amount of bombs it has around it
-        self.numAround = 0
-        for i in self.numbers:
-            if i.getBomb():
-                self.numAround += 1 #If it has a bomb, the number will rise to the amount of bombs it has around it
